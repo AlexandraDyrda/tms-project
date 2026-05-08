5852d3cf1e5cd1771c7f58769013953e9c9eb405
Sasha <dyrdaalexandra@gmail.com>
2026-05-08 10:49:03 +0200

    fix: restore Task model for tests

--- src/models/task.py
+++ src/models/task.py
@@ -1,2 +1,24 @@
-def get_status() -> str:
-    return "TODO"
+from enum import Enum
+from typing import Dict, List, Any
+
+class TaskStatus(Enum):
+    TODO = 'todo'
+    IN_PROGRESS = 'in_progress'
+    DONE = 'done'
+
+ALLOWED_TRANSITIONS: Dict[TaskStatus, List[TaskStatus]] = {
+    TaskStatus.TODO: [TaskStatus.IN_PROGRESS],
+    TaskStatus.IN_PROGRESS: [TaskStatus.DONE],
+    TaskStatus.DONE: []
+}
+
+class Task:
+    def __init__(self, id: Any, title: str, status: TaskStatus) -> None:
+        self.id = id
+        self.title = title
+        self.status = status
+
+    def change_status(self, new_status: TaskStatus) -> None:
+        if new_status not in ALLOWED_TRANSITIONS[self.status]:
+            raise ValueError(f'Cannot transition from {self.status} to {new_status}')
+        self.status = new_status
